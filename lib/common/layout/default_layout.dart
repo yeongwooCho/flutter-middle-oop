745a908b146fa1a38466ce6bee9e7282d092a3c6
yeongwoo <jyy0223@naver.com>
2023-05-24 19:12:39 +0900

    [add] default layout background color 지정 가능

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
+  final Color? backgroundColor;
   final Widget child;
 
   const DefaultLayout({
     required this.child,
+    this.backgroundColor,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // 기본배경이 완전 흰색은 아니다.
+      backgroundColor: backgroundColor ?? Colors.white, // 기본배경이 완전 흰색은 아니다.
       body: child,
     );
   }
