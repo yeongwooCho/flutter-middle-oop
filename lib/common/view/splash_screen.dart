f0a2bd2ba02f17e4fe33a73b035830f112885585
yeongwoo <jyy0223@naver.com>
2023-11-20 20:37:39 +0900

    [add] splash 에서 refresh token 재발급 후 localstorage 에 삽입

--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -37,6 +37,11 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       );
 
+      await storage.write(
+        key: ACCESS_TOKEN_KEY,
+        value: resp.data['accessToken'],
+      );
+
       Navigator.of(context).pushAndRemoveUntil(
         MaterialPageRoute(
           builder: (_) => RootTab(),
