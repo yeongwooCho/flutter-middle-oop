2447127f12bb340902eb01e85ae35ad1727c24c3
yeongwoo <jyy0223@naver.com>
2023-11-22 01:13:02 +0900

    [add] RestaurantDetailModel

---
+++ lib/restaurant/model/restaurant_detail_model.dart
@@ -0,0 +1,36 @@
+import 'package:code_factory_middle/restaurant/model/restaurant_model.dart';
+
+class RestaurantDetailModel extends RestaurantModel {
+  final String detail;
+  final List<RestaurantProductModel> products;
+
+  RestaurantDetailModel({
+    required super.id,
+    required super.name,
+    required super.thumbUrl,
+    required super.tags,
+    required super.priceRange,
+    required super.ratings,
+    required super.ratingsCount,
+    required super.deliveryTime,
+    required super.deliveryFee,
+    required this.detail,
+    required this.products,
+  });
+}
+
+class RestaurantProductModel {
+  final String id;
+  final String name;
+  final String imgUrl;
+  final String detail;
+  final int price;
+
+  RestaurantProductModel({
+    required this.id,
+    required this.name,
+    required this.imgUrl,
+    required this.detail,
+    required this.price,
+  });
+}
