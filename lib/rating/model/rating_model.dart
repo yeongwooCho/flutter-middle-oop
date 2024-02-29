40fd0165d732410a88c80aef86a04fa7e76f5609
yeongwoo <jyy0223@naver.com>
2024-02-29 15:46:39 +0900

    [add] rating model 구현

---
+++ lib/rating/model/rating_model.dart
@@ -0,0 +1,29 @@
+import 'package:code_factory_middle/common/utils/data_utils.dart';
+import 'package:code_factory_middle/user/model/user_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel {
+  final String id;
+  final UserModel user;
+  final int rating;
+  final String content;
+  @JsonKey(fromJson: DataUtils.listPathsToUrls)
+  final List<String> imgUrls;
+
+  RatingModel({
+    required this.id,
+    required this.user,
+    required this.rating,
+    required this.content,
+    required this.imgUrls,
+  });
+  
+  factory RatingModel.fromJson(Map<String, dynamic> json) => 
+      _$RatingModelFromJson(json);
+  
+  @override
+  Map<String, dynamic> toJson() => _$RatingModelToJson(this);
+}
