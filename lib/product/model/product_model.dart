9e1c786055bb5bef6d9ee2b8706f76bc6578724c
yeongwoo <jyy0223@naver.com>
2024-03-02 13:49:20 +0900

    [add] product model 정의

---
+++ lib/product/model/product_model.dart
@@ -0,0 +1,34 @@
+import 'package:code_factory_middle/common/model/model_with_id.dart';
+import 'package:code_factory_middle/common/utils/data_utils.dart';
+import 'package:code_factory_middle/restaurant/model/restaurant_detail_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product_model.g.dart';
+
+@JsonSerializable()
+class ProductModel implements IModelWithId {
+  final String id;
+  final String name;
+  @JsonKey(
+    fromJson: DataUtils.pathToUrl,
+  )
+  final String imgUrl;
+  final String detail;
+  final int price;
+  final RestaurantProductModel restaurant;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.imgUrl,
+    required this.detail,
+    required this.price,
+    required this.restaurant,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) =>
+      _$ProductModelFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$ProductModelToJson(this);
+}
