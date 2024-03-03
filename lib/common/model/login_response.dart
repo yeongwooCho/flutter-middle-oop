c883a3d8aa8d9ac2b54e8f33635da24ee9a48b9a
yeongwoo <jyy0223@naver.com>
2024-03-04 00:06:17 +0900

    [add] auth/login 과 auth/token 으로 응답받는 모델 생성

---
+++ lib/common/model/login_response.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_response.g.dart';
+
+@JsonSerializable()
+class LoginResponse {
+  final String accessToken;
+  final String refreshToken;
+
+  LoginResponse({
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) =>
+      _$LoginResponseFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$LoginResponseToJson(this);
+}
---
+++ lib/common/model/token_response.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'token_response.g.dart';
+
+@JsonSerializable()
+class TokenResponse {
+  final String accessToken;
+
+  TokenResponse({
+    required this.accessToken,
+  });
+
+  factory TokenResponse.fromJson(Map<String, dynamic> json) =>
+      _$TokenResponseFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$TokenResponseToJson(this);
+}
