63bb811e295a457459d663bc9e31dbb5cd17a3bb
yeongwoo <jyy0223@naver.com>
2024-03-04 17:39:54 +0900

    [add] screen 에 routeName 을 static 변수로 지정

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -11,6 +11,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
+  static String get routeName => 'login';
+
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
