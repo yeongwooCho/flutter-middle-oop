5000681b6ea8b80883d2edf2bc822a1038823fdb
yeongwoo <jyy0223@naver.com>
2024-03-04 22:22:15 +0900

    [add] 장바구니 provider 추가, 장바구니에 아이템 추가와 삭제 기능

---
+++ lib/user/provider/basket_provider.dart
@@ -0,0 +1,65 @@
+import 'package:code_factory_middle/product/model/product_model.dart';
+import 'package:code_factory_middle/user/model/basket_item_model.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final basketProvider =
+    StateNotifierProvider<BasketStateNotifier, List<BasketItemModel>>((ref) {
+  return BasketStateNotifier();
+});
+
+class BasketStateNotifier extends StateNotifier<List<BasketItemModel>> {
+  BasketStateNotifier() : super([]);
+
+  Future<void> addToBasket({
+    required ProductModel product,
+  }) async {
+    // 1) 아직 장바구니에 해당되는 상품이 없으면 장바구니에 상품을 추가한다.
+    // 2) 상품이 들어 있으면 장바구니에 있는 값에 +1 을 한다. ? count 수량 아니었음?
+
+    // null 이 아니면 존재한다.
+    final exists =
+        state.firstWhereOrNull((element) => element.product.id == product.id) !=
+            null;
+
+    if (exists) {
+      state = state
+          .map((e) =>
+              e.product.id == product.id ? e.copyWith(count: e.count + 1) : e)
+          .toList();
+    } else {
+      state = [
+        ...state,
+        BasketItemModel(product: product, count: 1),
+      ];
+    }
+  }
+
+  Future<void> removeFromBasket({
+    required ProductModel product,
+    bool isDelete = false,
+  }) async {
+    final exists =
+        state.firstWhereOrNull((element) => element.product.id == product.id) !=
+            null;
+
+    if (!exists) {
+      // 상품이 존재하지 않으면 즉시 함수를 반환하고 아무것도 하지 않는다.
+      return;
+    } else {
+      final existingProduct = state.firstWhere(
+        (element) => element.product.id == product.id,
+      ); // 에러 안남
+
+      if (existingProduct.count == 1 || isDelete) {
+        state =
+            state.where((element) => element.product.id != product.id).toList();
+      } else {
+        state = state
+            .map((e) =>
+                e.product.id == product.id ? e.copyWith(count: e.count - 1) : e)
+            .toList();
+      }
+    }
+  }
+}
