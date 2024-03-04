1d2d8f65ab3739cfd0327dc632f95efb8a8c9a35
yeongwoo <jyy0223@naver.com>
2024-03-04 17:42:32 +0900

    [add] main에 router 적용 및 routerProvider 를 생성하여 authProvider 에 변화가 생기면 redirect 를 실행하도록 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:code_factory_middle/common/view/splash_screen.dart';
+import 'package:code_factory_middle/common/provider/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,17 +13,19 @@ void main() {
 
 // primary로 관리하기
 // 나중에 route를 사용하기 할때 context를 사용해야 하기에 아래와 같이 관리한다.
-class _App extends StatelessWidget {
+class _App extends ConsumerWidget {
   const _App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'NotoSans',
       ),
-      home: SplashScreen(),
+      routerConfig: router,
     );
   }
 }
