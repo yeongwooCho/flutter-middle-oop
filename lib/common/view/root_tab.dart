ba5de48f1c87ef10b872c68ca53dda016058b89f
yeongwoo <jyy0223@naver.com>
2024-03-04 18:35:38 +0900

    [add] 4번째 ProfileScreen에 로그아웃 버튼 생성

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,6 +1,7 @@
 import 'package:code_factory_middle/common/const/colors.dart';
 import 'package:code_factory_middle/common/layout/default_layout.dart';
 import 'package:code_factory_middle/product/view/product_screen.dart';
+import 'package:code_factory_middle/user/view/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../restaurant/view/restaurant_screen.dart';
@@ -51,7 +52,7 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
           RestaurantScreen(),
           ProductScreen(),
           Container(child: Text('주문')),
-          Container(child: Text('프로필')),
+          ProfileScreen(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
