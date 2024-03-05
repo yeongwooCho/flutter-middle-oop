cd0b5a23d7a10210477ebbbd18acf2875351e468
yeongwoo <jyy0223@naver.com>
2024-03-05 13:00:07 +0900

    [add] 장바구니에서 추가할 아이템을 ProductCard 를 재활용해서 구현

--- lib/product/component/product_card.dart
+++ lib/product/component/product_card.dart
@@ -1,26 +1,37 @@
 import 'package:code_factory_middle/common/const/colors.dart';
 import 'package:code_factory_middle/product/model/product_model.dart';
 import 'package:code_factory_middle/restaurant/model/restaurant_detail_model.dart';
+import 'package:code_factory_middle/user/provider/basket_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends ConsumerWidget {
+  final String id;
   final Image image;
   final String name;
   final String detail;
   final int price;
+  final VoidCallback? onSubtract;
+  final VoidCallback? onAdd;
 
   const ProductCard({
     super.key,
+    required this.id,
     required this.image,
     required this.name,
     required this.detail,
     required this.price,
+    this.onSubtract,
+    this.onAdd,
   });
 
   factory ProductCard.fromProductModel({
     required ProductModel model,
+    VoidCallback? onSubtract,
+    VoidCallback? onAdd,
   }) {
     return ProductCard(
+      id: model.id,
       image: Image.network(
         model.imgUrl,
         width: 110.0,
@@ -30,13 +41,18 @@ class ProductCard extends StatelessWidget {
       name: model.name,
       detail: model.detail,
       price: model.price,
+      onSubtract: onSubtract,
+      onAdd: onAdd,
     );
   }
 
   factory ProductCard.fromRestaurantProductModel({
     required RestaurantProductModel model,
+    VoidCallback? onSubtract,
+    VoidCallback? onAdd,
   }) {
     return ProductCard(
+      id: model.id,
       image: Image.network(
         model.imgUrl,
         width: 110.0,
@@ -46,53 +62,145 @@ class ProductCard extends StatelessWidget {
       name: model.name,
       detail: model.detail,
       price: model.price,
+      onSubtract: onSubtract,
+      onAdd: onAdd,
     );
   }
 
   @override
-  Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: image,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final basket = ref.watch(basketProvider);
+
+    return Column(
+      children: [
+        IntrinsicHeight(
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: image,
+              ),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      detail,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(
+                        color: BODY_TEXT_COLOR,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    Text(
+                      'W$price',
+                      style: TextStyle(
+                        color: PRIMARY_COLOR,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(width: 16.0),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Text(
-                  detail,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: TextStyle(
-                    color: BODY_TEXT_COLOR,
-                    fontSize: 14.0,
-                  ),
-                ),
-                Text(
-                  'W$price',
-                  style: TextStyle(
-                    color: PRIMARY_COLOR,
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.right,
-                ),
-              ],
+        ),
+        if (onSubtract != null && onAdd != null)
+          _Footer(
+            total: (
+              basket.firstWhere((e) => e.product.id == id).count *
+                  basket.firstWhere((e) => e.product.id == id).product.price,
+            ).toString(),
+            count: basket.firstWhere((e) => e.product.id == id).count,
+            onSubtract: onSubtract!,
+            onAdd: onAdd!,
+          ),
+      ],
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final String total;
+  final int count;
+  final VoidCallback onSubtract;
+  final VoidCallback onAdd;
+
+  const _Footer({
+    super.key,
+    required this.total,
+    required this.count,
+    required this.onSubtract,
+    required this.onAdd,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            '총액 ₩$total원',
+            style: const TextStyle(
+              color: PRIMARY_COLOR,
+              fontWeight: FontWeight.w500,
             ),
           ),
-        ],
+        ),
+        Row(
+          children: [
+            renderButton(
+              icon: Icons.remove,
+              onTap: onSubtract,
+            ),
+            Text(
+              count.toString(),
+              style: const TextStyle(
+                color: PRIMARY_COLOR,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            renderButton(
+              icon: Icons.add,
+              onTap: onAdd,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget renderButton({
+    required IconData icon,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      // 테투리 용
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Icon(
+          icon,
+          color: PRIMARY_COLOR,
+        ),
       ),
     );
   }
