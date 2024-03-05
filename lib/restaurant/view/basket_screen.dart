8a2c45ebe6facdd56bf5cdbffc137449ebb68538
yeongwoo <jyy0223@naver.com>
2024-03-05 12:59:38 +0900

    [add] 장바구니 스크린 생성 후 routes 에 route 추가

---
+++ lib/restaurant/view/basket_screen.dart
@@ -0,0 +1,19 @@
+import 'package:code_factory_middle/common/layout/default_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class BasketScreen extends ConsumerWidget {
+  static String get routeName => 'basket';
+
+  const BasketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return DefaultLayout(
+      title: '장바구니',
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
