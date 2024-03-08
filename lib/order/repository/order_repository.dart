9afd03b0ff8a09c2f6ebc80a1852b2d09d55b43c
yeongwoo <jyy0223@naver.com>
2024-03-08 22:47:49 +0900

    [add] orderRepositoryProvider 정의

--- lib/order/repository/order_repository.dart
+++ lib/order/repository/order_repository.dart
@@ -1,9 +1,18 @@
+import 'package:code_factory_middle/common/const/data.dart';
+import 'package:code_factory_middle/common/repository/dio/dio.dart';
 import 'package:code_factory_middle/order/model/order_model.dart';
 import 'package:code_factory_middle/order/model/post_order_body.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'order_repository.g.dart';
+
+final orderRepositoryProvider = Provider<OrderRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return OrderRepository(dio, baseUrl: 'http://$ip/order');
+});
 
 @RestApi()
 abstract class OrderRepository {
